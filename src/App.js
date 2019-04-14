9980476e2180d8b901cee0b14b870097e445820d
Zevi Liao <jointjm@gmail.com>
2019-04-14 13:11:02 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg';
+import './App.css'
+import Hello from './componets/hello'
+
+const a = <h1>hello</h1>
+
+console.log('a', a)
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Hello />
   }
 }
 
-export default App;
+export default App

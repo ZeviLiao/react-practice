1d18570ed3fdc96b7f5c84b84d1fd4d07925b85b
Zevi Liao <jointjm@gmail.com>
2019-04-15 00:21:17 +0800

    delete

--- src/componets/counter.jsx
+++ src/componets/counter.jsx
@@ -14,7 +14,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.props.children}
         <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
@@ -25,6 +24,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     )

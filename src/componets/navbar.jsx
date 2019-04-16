d8384ca91c93f7f451498ce7ea645ec319494aab
Zevi Liao <jointjm@gmail.com>
2019-04-16 12:53:04 +0800

    this pros

--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <span className="badge badge-pill badge-secondary">
-          {this.props.totalCounters}
-        </span>
-      </nav>
-    )
-  }
+const NavBar = props => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar
+      </a>
+      <span className="badge badge-pill badge-secondary">
+        {props.totalCounters}
+      </span>
+    </nav>
+  )
 }
 
 export default NavBar
